56ebb6528e671319df5192a144c0022129f0d229
jw_mac <77239050qq@gmail.com>
2026-04-13 18:31:34 +0800

    recover

--- exercises/06_stack_maze/06_stack_maze.c
+++ exercises/06_stack_maze/06_stack_maze.c
@@ -1,67 +1,69 @@
 #include <stdio.h>
-#include<stdbool.h>
+#include <stdbool.h>
 
 #define MAX_ROW 5
 #define MAX_COL 5
 #define MAX_MATRIC (MAX_ROW * MAX_COL)
 
 int maze[MAX_ROW][MAX_COL] = {
-	0, 1, 0, 0, 0,
-	0, 1, 0, 1, 0,
-	0, 0, 0, 0, 0,
-	0, 1, 1, 1, 0,
-	0, 0, 0, 1, 0,
+    0, 1, 0, 0, 0,
+    0, 1, 0, 1, 0,
+    0, 0, 0, 0, 0,
+    0, 1, 1, 1, 0,
+    0, 0, 0, 1, 0,
 };
 
 typedef struct 
 {
-	int r;
-	int c;
-}Point;
+    int r;
+    int c;
+} Point;
 
-int dr[] = {0,1,-1,0};
-int dc[] = {1,0,0,-1};
+int dr[] = {0, 1, -1, 0};
+int dc[] = {1, 0, 0, -1};
 
 typedef struct 
 {
-	Point data[MAX_MATRIC];
-	int top;
-}Stack;
+    Point data[MAX_MATRIC];
+    int top;
+} Stack;
 
 void init_stack(Stack *s){
-	s->top = -1;
+    s->top = -1;
 }
 
 bool empty_stack(Stack *s){
-	return s->top == -1;
+    return s->top == -1;
 }
-bool push_stack(Stack *s,Point p){
-	if(s->top >= MAX_MATRIC-1){
-		return false;
-	}
-	s->top++;
-	s->data[s->top] = p;
-	return true;
+
+bool push_stack(Stack *s, Point p){
+    if(s->top >= MAX_MATRIC - 1){
+        return false;
+    }
+    s->top++;
+    s->data[s->top] = p;
+    return true;
 }
+
 Point pop_stack(Stack *s){
-	if(s->top> MAX_MATRIC-1||empty_stack(s)){
-		Point temp = {-1,-1};
-		return temp;
-	}
-	Point p = s->data[s->top];
-	s->top--;
-	return p;
+    if(s->top > MAX_MATRIC - 1 || empty_stack(s)){
+        Point temp = {-1, -1};
+        return temp;
+    }
+    Point p = s->data[s->top];
+    s->top--;
+    return p;
 }
-bool isvalid(int r,int c,bool visit[MAX_ROW][MAX_COL]){
-	if(r<MAX_ROW&&c<MAX_COL&&!visit[r][c]&&maze[r][c]== 0){
-		return true;
-	}
-	return false;
+
+bool isvalid(int r, int c, bool visit[MAX_ROW][MAX_COL]){
+    if(r >= 0 && r < MAX_ROW && c >= 0 && c < MAX_COL && !visit[r][c] && maze[r][c] == 0){
+        return true;
+    }
+    return false;
 }
-// 修改你的 dfs 函数
+
 bool dfs(Point start, Point end){
     bool visit[MAX_ROW][MAX_COL] = {false};
-    
     Point parent[MAX_ROW][MAX_COL]; 
     
     Stack s;
@@ -71,13 +73,21 @@ bool dfs(Point start, Point end){
     
     while(!empty_stack(&s)){
         Point cur = pop_stack(&s);
+        
         if(cur.c == end.c && cur.r == end.r){
+            Point path[MAX_MATRIC];
+            int path_len = 0;
             Point p = end;
+            
             while (!(p.r == start.r && p.c == start.c)) {
-                printf("(%d, %d)\n", p.r, p.c); 
+                path[path_len++] = p;
                 p = parent[p.r][p.c]; 
             }
-            printf("(%d, %d)\n", start.r, start.c); 
+            path[path_len++] = start; 
+            
+            for(int i = path_len - 1; i >= 0; i--){
+                printf("(%d, %d)\n", path[i].r, path[i].c);
+            }
             return true;
         }
         
@@ -87,10 +97,8 @@ bool dfs(Point start, Point end){
             
             if(isvalid(next_r, next_c, visit)){
                 visit[next_r][next_c] = true; 
-                
                 Point next = {next_r, next_c};
                 parent[next_r][next_c] = cur; 
-                
                 push_stack(&s, next);
             }
         }
@@ -98,14 +106,14 @@ bool dfs(Point start, Point end){
     return false;
 }
 
-
-
 int main(void)
 {
-	Point start = {0,0};
-	Point end = {4,4};
-	if(dfs(start,end)){
-		printf("succeed");
-	}
-	return 0;
+    Point start = {0, 0};
+    Point end = {4, 4};
+    if(dfs(start, end)){
+        printf("succeed\n"); 
+    } else {
+        printf("failed\n");
+    }
+    return 0;
 }
