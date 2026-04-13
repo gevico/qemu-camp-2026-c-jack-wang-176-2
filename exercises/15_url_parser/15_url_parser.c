616f73f166fa77a27ca3b35e71bfa868efd99958
jw_mac <77239050qq@gmail.com>
2026-04-13 20:40:44 +0800

    15 finish

--- exercises/15_url_parser/15_url_parser.c
+++ exercises/15_url_parser/15_url_parser.c
@@ -11,9 +11,30 @@
 
 int parse_url(const char* url) {
     int err = 0;
-
-
+    char* temp_url =strdup(url);
+    if(temp_url == NULL){
+        err = -EINVAL;
+        goto exit;
+    }
+    char* query_start = strchr(temp_url,'?');
+    if(query_start == NULL){
+        goto exit;
+    }
+    query_start ++;
+    char* pair = strtok(query_start,"&");
+    while(pair != NULL){
+        char* equal =strchr(pair,'=');
+        if(equal != NULL){
+            *equal = '\0';
+            char* key = pair;
+            char* value = equal +1;
+            printf("key = %s, value = %s\n", key, value);
+        }
+        pair = strtok(NULL,"&");
+    }
 exit:
+    if(temp_url)
+    free(temp_url);
     return err;
 }
 
