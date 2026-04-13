7dfadf9c70f0d6385b58c8924e47bf16629e68a6
jw_mac <77239050qq@gmail.com>
2026-04-13 15:06:26 +0800

    09 finish

--- exercises/09_word_counter/09_word_counter.c
+++ exercises/09_word_counter/09_word_counter.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
+#include<stdbool.h>
+
+int count_word(const char* str){
+    int count =0;
+    bool is_word = false;
+    while(*str != '\0'){
+        if(*str == '\n'||*str =='\t'||*str==' '){
+            is_word = false;
+
+        }else if(!is_word){
+            is_word = true;
+            count ++;
+        }
+        str++;
+    }
+    return count;
+}
+
 
 int main() {
     char str[]="Don't ask what your country can do for you, but ask what you can do for your country.";
     int wordCount = 0;
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
     
+    wordCount = count_word(str);
     printf("单词数量: %d\n", wordCount);
     
     return 0;
