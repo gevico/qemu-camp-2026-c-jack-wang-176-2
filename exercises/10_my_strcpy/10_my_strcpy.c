c4d40a676e5f1a4bd5793d800c5210911d9c17b8
jw_mac <77239050qq@gmail.com>
2026-04-13 15:19:57 +0800

    10 finish

--- exercises/10_my_strcpy/10_my_strcpy.c
+++ exercises/10_my_strcpy/10_my_strcpy.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
+void strcpy(char* dest,const char* src){
+    while(*src != '\0'){
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest='\0';
+}
+
 int main() {
     char source[] = "Hello, World! This is a test string for my_strcpy function.\n";
     char destination[101];
     
-	// TODO: 在这里添加你的代码
-    // I AM NOT DONE
+	strcpy(destination,source);
     
     printf("拷贝后的字符串: %s", destination);
     
