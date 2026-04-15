e02014ae90ae3717f013dc76b86efe20cc552473
jw_mac <77239050qq@gmail.com>
2026-04-15 14:36:10 +0800

    17 finish

--- exercises/17_myfile/17_myfile.c
+++ exercises/17_myfile/17_myfile.c
@@ -8,9 +8,13 @@
 void print_elf_type(uint16_t e_type) {
   const char *type_str;
   switch (e_type) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
-  }
+        case ET_NONE: type_str = "No file type"; break;
+        case ET_REL:  type_str = "Relocatable"; break; 
+        case ET_EXEC: type_str = "Executable"; break;
+        case ET_DYN:  type_str = "Shared Object/PIE"; break; 
+        case ET_CORE: type_str = "Core file"; break;
+        default:      type_str = "Unknown type"; break;
+    }
   printf("ELF Type: %s (0x%x)\n", type_str, e_type);
 }
 
@@ -23,8 +27,23 @@ int main(int argc, char *argv[]) {
   int fd;
   Elf64_Ehdr ehdr;
 
-  // TODO: 在这里添加你的代码
-  // I AM NOT DONE
+  for(int i=0;i<2;i++){
+    fd =open(filepath[i],O_RDONLY);
+    if(fd<0){
+      continue;
+    }
+    ssize_t file_size = read(fd,&ehdr,sizeof(Elf64_Ehdr));
+    if(file_size<0){
+      close(fd);
+      continue;
+    }
+    if((memcmp(ehdr.e_ident,ELFMAG, SELFMAG))!= 0){
+      close(fd);
+      continue;
+    }
+    print_elf_type(ehdr.e_type);
+    close(fd);
+  }
   
   return 0;
 }
