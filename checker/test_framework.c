14bf1588c330bb7691eb5bf6cbf21cb1bd1caed8
jw_mac <77239050qq@gmail.com>
2026-04-18 18:22:03 +0800

    1234123

--- checker/test_framework.c
+++ checker/test_framework.c
@@ -118,7 +118,7 @@ int compile_and_run(const char* source_file, const char* executable, char* outpu
     switch (is_make) {
         case 2: {
             // 🚨 修正：脚本就在练习题目录下，直接运行即可
-            snprintf(run_cmd, sizeof(run_cmd), "cd ../exercises/%s && bash ./run_my_test.sh 2>&1", executable);
+            snprintf(run_cmd, sizeof(run_cmd), "cd ../exercises/%s && bash ./test_20_mybash.sh 2>&1", executable);
             fp = popen(run_cmd, "r");
             if (fp == NULL) {
                 snprintf(output, output_size, "无法执行运行命令: %s", run_cmd);
