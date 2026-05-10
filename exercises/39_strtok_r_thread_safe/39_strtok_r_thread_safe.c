ec85af9b54790c56df117cfa80a1b1595489d9c6
jw_mac <77239050qq@gmail.com>
2026-05-10 16:26:15 +0800

    39 finish

--- exercises/39_strtok_r_thread_safe/39_strtok_r_thread_safe.c
+++ exercises/39_strtok_r_thread_safe/39_strtok_r_thread_safe.c
@@ -10,14 +10,43 @@
 
 /* 判断字符 c 是否在分隔符集合 delim 中 */
 static int is_delim(char c, const char *delim) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    while(*delim != '\0'){
+        if(c == *delim){
+            return 1;
+        }
+        delim++;
+    }
+    return 0;
 }
 
 /* 线程安全版本：通过 saveptr 维护调用状态，不使用静态变量 */
 char *strtok_r(char *str, const char *delim, char **saveptr) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    char* start_token;
+    if(str == NULL){
+        str = *saveptr;
+    }
+    if(str == NULL){
+        return NULL;
+    }
+    while(is_delim(*str,delim)&&*str != '\0'){
+        str++;
+    }
+    if(*str=='\0'){
+        *saveptr = NULL;
+        return NULL;
+    }
+    start_token= str;
+    while(!is_delim(*str,delim)&&*str != '\0'){
+        str++;
+    }
+    if(*str=='\0'){
+        *saveptr = NULL;
+        return start_token;
+    }else{
+        *str ='\0';
+        *saveptr = str+1;
+    }
+    return start_token;
 }
 
 int main(void) {
